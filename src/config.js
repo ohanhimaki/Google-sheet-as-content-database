05040ddac75657f510b71715a4425ae48984caa5
ohanhimaki <ohanhimaki@gmail.com>
2018-12-12 15:45:01 +0200

    SheetsAPI is workning

---
+++ src/config.js
@@ -0,0 +1,6 @@
+export default {
+    apiKey: "AIzaSyCzFsyrBNkN92LC8lU5NE7gXRT5vqnT1Yg",
+    discoveryDocs:
+        ["https://sheets.googleapis.com/$discovery/rest?version=v4"],
+    spreadsheetId: "1ne2sQvfk11bC2fEpgOMkTlOEdyWTRtHJS8i43jWrjjU"
+};
